59928c24b0abed3b6f0bea1754683b3dc468f6cd
EvgenyGRI <eugene_gri@mail.ru>
2023-11-30 12:42:22 +0300

    define PROTO:GIDR-SG-<IN|E>GRESS rules API

--- pkg/api/common/ip-transport.pb.go
+++ pkg/api/common/ip-transport.pb.go
@@ -71,6 +71,56 @@ func (IpAddrFamily) EnumDescriptor() ([]byte, []int) {
 	return file_common_ip_transport_proto_rawDescGZIP(), []int{0}
 }
 
+// Ingress / Egress traffic dir
+type Traffic int32
+
+const (
+	Traffic_Undef   Traffic = 0 // undefined tfaffic direction
+	Traffic_Ingress Traffic = 1 // Ingress
+	Traffic_Egress  Traffic = 2 // Egress
+)
+
+// Enum value maps for Traffic.
+var (
+	Traffic_name = map[int32]string{
+		0: "Undef",
+		1: "Ingress",
+		2: "Egress",
+	}
+	Traffic_value = map[string]int32{
+		"Undef":   0,
+		"Ingress": 1,
+		"Egress":  2,
+	}
+)
+
+func (x Traffic) Enum() *Traffic {
+	p := new(Traffic)
+	*p = x
+	return p
+}
+
+func (x Traffic) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (Traffic) Descriptor() protoreflect.EnumDescriptor {
+	return file_common_ip_transport_proto_enumTypes[1].Descriptor()
+}
+
+func (Traffic) Type() protoreflect.EnumType {
+	return &file_common_ip_transport_proto_enumTypes[1]
+}
+
+func (x Traffic) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use Traffic.Descriptor instead.
+func (Traffic) EnumDescriptor() ([]byte, []int) {
+	return file_common_ip_transport_proto_rawDescGZIP(), []int{1}
+}
+
 //Transport: represents net transport (L4)
 type Networks_NetIP_Transport int32
 
@@ -102,11 +152,11 @@ func (x Networks_NetIP_Transport) String() string {
 }
 
 func (Networks_NetIP_Transport) Descriptor() protoreflect.EnumDescriptor {
-	return file_common_ip_transport_proto_enumTypes[1].Descriptor()
+	return file_common_ip_transport_proto_enumTypes[2].Descriptor()
 }
 
 func (Networks_NetIP_Transport) Type() protoreflect.EnumType {
-	return &file_common_ip_transport_proto_enumTypes[1]
+	return &file_common_ip_transport_proto_enumTypes[2]
 }
 
 func (x Networks_NetIP_Transport) Number() protoreflect.EnumNumber {
@@ -341,11 +391,14 @@ var file_common_ip_transport_proto_rawDesc = []byte{
 	0x0a, 0x05, 0x54, 0x79, 0x70, 0x65, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0d, 0x52, 0x05, 0x54,
 	0x79, 0x70, 0x65, 0x73, 0x2a, 0x29, 0x0a, 0x0c, 0x49, 0x70, 0x41, 0x64, 0x64, 0x72, 0x46, 0x61,
 	0x6d, 0x69, 0x6c, 0x79, 0x12, 0x05, 0x0a, 0x01, 0x5f, 0x10, 0x00, 0x12, 0x08, 0x0a, 0x04, 0x49,
-	0x50, 0x76, 0x34, 0x10, 0x01, 0x12, 0x08, 0x0a, 0x04, 0x49, 0x50, 0x76, 0x36, 0x10, 0x02, 0x42,
-	0x2e, 0x5a, 0x2c, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x48, 0x2d,
-	0x42, 0x46, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x61, 0x70,
-	0x69, 0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x3b, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x50, 0x76, 0x34, 0x10, 0x01, 0x12, 0x08, 0x0a, 0x04, 0x49, 0x50, 0x76, 0x36, 0x10, 0x02, 0x2a,
+	0x2d, 0x0a, 0x07, 0x54, 0x72, 0x61, 0x66, 0x66, 0x69, 0x63, 0x12, 0x09, 0x0a, 0x05, 0x55, 0x6e,
+	0x64, 0x65, 0x66, 0x10, 0x00, 0x12, 0x0b, 0x0a, 0x07, 0x49, 0x6e, 0x67, 0x72, 0x65, 0x73, 0x73,
+	0x10, 0x01, 0x12, 0x0a, 0x0a, 0x06, 0x45, 0x67, 0x72, 0x65, 0x73, 0x73, 0x10, 0x02, 0x42, 0x2e,
+	0x5a, 0x2c, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x48, 0x2d, 0x42,
+	0x46, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x61, 0x70, 0x69,
+	0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x3b, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -360,15 +413,16 @@ func file_common_ip_transport_proto_rawDescGZIP() []byte {
 	return file_common_ip_transport_proto_rawDescData
 }
 
-var file_common_ip_transport_proto_enumTypes = make([]protoimpl.EnumInfo, 2)
+var file_common_ip_transport_proto_enumTypes = make([]protoimpl.EnumInfo, 3)
 var file_common_ip_transport_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_common_ip_transport_proto_goTypes = []interface{}{
 	(IpAddrFamily)(0),                // 0: common.IpAddrFamily
-	(Networks_NetIP_Transport)(0),    // 1: common.Networks.NetIP.Transport
-	(*Networks)(nil),                 // 2: common.Networks
-	(*ICMP)(nil),                     // 3: common.ICMP
-	(*Networks_NetIP)(nil),           // 4: common.Networks.NetIP
-	(*Networks_NetIP_PortRange)(nil), // 5: common.Networks.NetIP.PortRange
+	(Traffic)(0),                     // 1: common.Traffic
+	(Networks_NetIP_Transport)(0),    // 2: common.Networks.NetIP.Transport
+	(*Networks)(nil),                 // 3: common.Networks
+	(*ICMP)(nil),                     // 4: common.ICMP
+	(*Networks_NetIP)(nil),           // 5: common.Networks.NetIP
+	(*Networks_NetIP_PortRange)(nil), // 6: common.Networks.NetIP.PortRange
 }
 var file_common_ip_transport_proto_depIdxs = []int32{
 	0, // 0: common.ICMP.IPv:type_name -> common.IpAddrFamily
@@ -439,7 +493,7 @@ func file_common_ip_transport_proto_init() {
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_common_ip_transport_proto_rawDesc,
-			NumEnums:      2,
+			NumEnums:      3,
 			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
